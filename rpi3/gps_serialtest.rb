c6407e8bee657db5ad14597165524944f806175f
ikegawa-koshi <ikegawa.koshi@gmail.com>
2017-08-04 10:54:33 +0900

    fix gps serial

--- rpi3/gps_serialtest.rb
+++ rpi3/gps_serialtest.rb
@@ -9,15 +9,25 @@ sp = SerialPort.new('/dev/serial0', 9600, 8, 1, 0)
 sp.puts "serial0 open"
 
 # echo loop
-loop do
-  line = sp.gets
+begin
+    CSV.open('data_all.csv','w') do |row|
+    end
 
-  newLine = line.chomp
-  strArr = newLine.split(",")
-  if strArr[0] == '$GPGGA' then
-    newStr = strArr[0] + ',' + strArr[2] + ',' + strArr[4]
-    puts newStr
-    #sp.puts line
-  end
+    loop do
+        line = sp.gets
+        newLine = line.chomp
+        strArr = newLine.split(",")
+        if strArr[0] == '$GPGGA' then
+            newStr = strArr[0] + ',' + strArr[2] + ',' + strArr[4]
+            puts newStr
+            #sp.puts line
+        end
+
+        CSV.open('data_all.csv','a') do |csv|
+            csv << strArr
+        end
+    end
+rescue Interrupt
+    sp.close
+    exit 0
 end
-sp.close
